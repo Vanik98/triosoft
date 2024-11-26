36baf47a0148c7fa44c31fed4edf5647a90d730e
Vanik Dallakyan <dallakyanvanik@gmail.com>
2024-11-26 20:54:01 +0400

    add translations

---
+++ lib/generated/codegen_loader.g.dart
@@ -0,0 +1,69 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: prefer_single_quotes, avoid_renaming_method_parameters
+
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart' show AssetLoader;
+
+class CodegenLoader extends AssetLoader{
+  const CodegenLoader();
+
+  @override
+  Future<Map<String, dynamic>?> load(String path, Locale locale) {
+    return Future.value(mapLocales[locale.toString()]);
+  }
+
+  static const Map<String,dynamic> hy = {
+  "sign_up": "Գրանցվել",
+  "quick_easy": "Արագ և Հեշտ է",
+  "name": "Անուն",
+  "email": "Էլ. հասցե",
+  "number": "Հեռախոսահամար",
+  "region": "Տարածաշրջան",
+  "city_village": "Քաղաք, Գյուղ",
+  "school": "Դպրոց",
+  "subject": "Առարկա",
+  "grade": "Դասարան",
+  "password": "Գաղտնաբառ",
+  "confirm_password": "Հաստատել գաղտնաբառը",
+  "ok": "Լավ",
+  "error": "սխալ",
+  "log_out": "Դուրս գալ"
+};
+static const Map<String,dynamic> ru = {
+  "sign_up": "Зарегистрироваться",
+  "quick_easy": "Быстро и легко",
+  "name": "Имя",
+  "email": "Эл. почта",
+  "number": "Номер",
+  "region": "Регион",
+  "city_village": "Город, Деревня",
+  "school": "Школа",
+  "subject": "Предмет",
+  "grade": "Класс",
+  "password": "Пароль",
+  "confirm_password": "Подтвердить пароль",
+  "ok": "Ок",
+  "error": "ошибка",
+  "log_out": "Выйти"
+};
+static const Map<String,dynamic> en = {
+  "sign_up": "Sign Up",
+  "quick_easy": "It is Quick and Easy",
+  "name": "Name",
+  "email": "Email",
+  "number": "Number",
+  "region": "Region",
+  "city_village": "City, Village",
+  "school": "School",
+  "subject": "Subject",
+  "grade": "Grade",
+  "password": "Password",
+  "confirm_password": "Confirm Password",
+  "ok": "Ok",
+  "error": "error",
+  "log_out": "Log Out"
+};
+static const Map<String, Map<String,dynamic>> mapLocales = {"hy": hy, "ru": ru, "en": en};
+}
---
+++ lib/generated/locale_keys.g.dart
@@ -0,0 +1,20 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const sign_up = 'sign_up';
+  static const quick_easy = 'quick_easy';
+  static const name = 'name';
+  static const email = 'email';
+  static const number = 'number';
+  static const region = 'region';
+  static const city_village = 'city_village';
+  static const school = 'school';
+  static const subject = 'subject';
+  static const grade = 'grade';
+  static const password = 'password';
+  static const confirm_password = 'confirm_password';
+  static const ok = 'ok';
+  static const error = 'error';
+  static const log_out = 'log_out';
+
+}
